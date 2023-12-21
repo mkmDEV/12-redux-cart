4b9fb7648f490725c62fa81768144ace11135cf8
mkmDEV <mkmdev18@gmail.com>
2023-12-21 20:28:25 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Layout from '@components/Layout/Layout';
+import Cart from '@components/Cart/Cart';
+import Products from '@components/Shop/Products';
+
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,18 @@
+import Card from '@components/UI/Card';
+import CartItem from '@components/Cart/CartItem';
+
+import classes from './Cart.module.css';
+const Cart = (props) => {
+  return (
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        <CartItem
+          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
+        />
+      </ul>
+    </Card>
+  );
+};
+
+export default Cart;
